c8bed5d4791b5f151ed22dbed3ff6f1e93b81105
Ochwada <Ochwada@users.noreply.github.com>
2023-01-12 15:51:30 +0100

    configure lodash

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname +'/date.js');
+const _ = require('lodash');
 
 const app = express();
 
