0ae7555d460895fc5ff86a19be54c2c156a77959
Ochwada <Ochwada@users.noreply.github.com>
2023-01-13 17:18:15 +0100

    todo app with mongoDB2

--- todolist-withDB/app.js
+++ todolist-withDB/app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const _ = require('lodash');
 const mongoose = require('mongoose');
 
-const date = require(__dirname + '/date.js');
+//const date = require(__dirname + '/date.js');
 const app = express();
 
 
@@ -26,30 +26,61 @@ mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
 
 // ---------Mongo Schema -------- 
 const itemsSchema = new mongoose.Schema({
-    name: 'String'
+    name: String
 });
 
 // ---------Mongo model -------- 
 
-const Item = mongoose.model("Item",itemsSchema );
+const Item = mongoose.model("Item", itemsSchema);
 
-//const item = new
 
+// add documents ---
+const cook = new Item({
+    name: "cook"
+});
+const clean = new Item({
+    name: "clean"
+});
+const wash = new Item({
+    name: "wash"
+});
+
+const defaultItems = [cook, clean, wash]
+/*
+Item.insertMany(defaultItems, (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("successful inputs")
+    }
+}) */
+
+Item.find((err, items) => {
+    if (err) {
+        console.log(err);
+    } else {
+        items.forEach((item) => {
+            console.log(item.name)
+        })
+    }
+}
+);
 // --------------------
 
 app.get('/', (req, res) => {
 
-    let day = date.getDate();
+// check if  item collection is empty
+
+    Item.find({}, (err, foundItems) => {
+        //console.log(foundItems);
+        res.render("list", {
+            // kindOfDay: day,
+            listTitle: "Today",
+            newListItems: foundItems
+        });
+    })
+
     
-  
-
-    res.render("list", {
-        // kindOfDay: day,
-        listTitle:day,
-        newListItems: items,
-    });
-    //res.render("list", { kindOfDay: day }) // "list" is an ejs file from view directory
-
 });
 
 
@@ -57,9 +88,9 @@ app.get('/', (req, res) => {
 app.post("/", (req, res) => {
     var item = req.body.newItem;
 
-    if (req.body.list ==="Work"){
+    if (req.body.list === "Work") {
         workItems.push(item);
-    }else{
+    } else {
         items.push(item);
         //console.log(item)
         res.redirect("/")
@@ -69,7 +100,7 @@ app.post("/", (req, res) => {
 
 // ------------------------------------------->
 
-app.get("/work",(req, res)=> {
+app.get("/work", (req, res) => {
     res.render("list", {
         listTitle: "Work List",
         newListItems: workItems
@@ -85,7 +116,7 @@ app.get("/about", (req, res) => {
 const port = 3000;
 
 app.listen(port, () =>
-    console.log('Server started on port ' + port)); 
+    console.log('Server started on port ' + port));
 
 /* app.listen(process.env.PORT, function () {
     console.log("Server running on Port (pre assigned)")
