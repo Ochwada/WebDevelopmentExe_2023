baa4103f264e6813b0f9d3c92d6240691b4a86ae
Ochwada <nakhulolinda@yahoo.com>
2023-01-18 22:11:36 +0100

    add mongo'

--- Blog_Website_v2/app.js
+++ Blog_Website_v2/app.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
-
+// packages 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
 
 const homeStartingContent =
     "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -13,13 +15,29 @@ const contactContent =
     "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+
+const url = "mongodb://localhost:27017/blogDB";
+
+mongoose.set('strictQuery', true);
+mongoose.connect(url, { useNewUrlParser: true });
 //---------------------Global Variables--------------------
-let posts = [];
-//-----------------------END---------------------------
+
+let post = [];
+//----------------------- ** ---------------------------
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+//---------------------Mongoose Schemas--------------------
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+//---------------------Mongoose Model--------------------
+
+const Post = mongoose.model('Post', postSchema);
+//----------------------- ** ---------------------------
 
 //--------------------------------------------------
 // Navigate to "pages" - in the "view directory"
@@ -28,11 +46,15 @@ app.use(express.static("public"));
 //---------------------home page--------------------
 
 app.get("/", (req, res) => {
-    res.render("home", {
-        homeStartingContent: homeStartingContent,
-        posts: posts
+    Post.find({}, (err, posts) => {
+        res.render("home", {
+            homeStartingContent: homeStartingContent,
+            posts: posts
 
-    });
+        });
+    })
+
+
 });
 
 //---------------------about page--------------------
@@ -59,25 +81,18 @@ app.get("/compose", (req, res) => {
 });
 //---------------------blog page--------------------
 
-app.get("/posts/:postName", (req, res) => {
-    let urlBlogName =_.startCase(req.params.postName); //used -lodash-
-    
+app.get("/posts/:postId", (req, res) => {
+    const requestedPostId = req.params.postId;
 
-    posts.forEach((post) => {
-        let storedTitle = _.startCase(post.title);
-
-        // -----open each blog -----
-
-        if (urlBlogName === storedTitle) {
-            res.render("post", {
-                title: post.title,
-                content: post.content
-            });
-        } 
+    Post.findOne({ _id: requestedPostId }, (err, post) => {
+        res.render("post", {
+            title: post.title,
+            content: post.content
+        });
     });
-
-    
 });
+
+
 
 //--------------------------------------------------
 // END
@@ -86,14 +101,15 @@ app.get("/posts/:postName", (req, res) => {
 // Post Requests/ Methods
 //--------------------------------------------------
 app.post("/compose", (req, res) => {
-    let post = {
-        title:req.body.postTitle, //used -lodash-
-        content: req.body.postInput
-    }
-
-    posts.push(post);
-
-    res.redirect("/")
+    const post = new Post({
+        title: req.body.postTitle, //used -lodash-
+        content: req.body.postBody
+    });
+    post.save((err) => {
+        if (!err) {
+            res.redirect("/")
+        }
+    });
 });
 
 
