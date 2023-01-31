6392a8fdc2781dc4ee7493aa068012c3a48c38cc
Ochwada <nakhulolinda@yahoo.com>
2023-01-31 17:44:03 +0100

    Get route to articles

--- wiki-API/app.js
+++ wiki-API/app.js
@@ -6,20 +6,44 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+
 // setting up the MongoDB
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost/wikiDB", () => {
-  console.log("Connected to MongoDB");
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", () => {
+    console.log("Connected to MongoDB");
 });
-        
-// Display  --> server code
-app.get('/', (req, res) => {
-  res.send('Hello Linda!');
-});
+// -----------------------SCHEMA---------------------------------------
+// create a schema to the document
+const articleSchema = {
+    title: String,
+    content: String
+}
+
+const Article = mongoose.model("Article", articleSchema);
 
 
+
+
+// ------------------------DISPLAY i.e Get route that fetches all the Articles --------------------------------------
+// Display  --> Server Code
+app.get('/articles', (req, res) => {
+    Article.find( (err, foundArticles) =>{
+        res.send(foundArticles)
+    });
+});
+
+// ------------------------HOST--------------------------------------
 // Host port 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
