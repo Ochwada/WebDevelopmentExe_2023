1168858e7414801909efaa4923efbe4c49cc05a1
Ochwada <nakhulolinda@yahoo.com>
2023-01-31 23:06:18 +0100

    npm init and package installation

--- Authentication/app.js
+++ Authentication/app.js
@@ -1,1 +1,39 @@
-//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+
+// --- setting up the MongoDB --
+mongoose.set("strictQuery", false);
+
+mongoose.connect("mongodb://localhost:27017/", () => {
+    console.log("Connected to MongoDB");
+});
+
+
+
+
+
+
+
+
+
+
+// ------------------------HOST--------------------------------------
+// Host port 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
