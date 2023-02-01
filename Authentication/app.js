2cb2b02cf1694b02b87236cbeecb29f1bfac4c7a
Ochwada <nakhulolinda@yahoo.com>
2023-02-01 15:18:17 +0100

    using passport.js to Add Cookies

--- Authentication/app.js
+++ Authentication/app.js
@@ -5,15 +5,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 
-// const md5 = require('md5'); // hashing password
-
-const bcrypt = require('bcrypt'); // Salting and Hashing Passwords with bcrypt
-const saltRounds = 10;
-
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 
 // security
-const encrypt = require('mongoose-encryption');
 const app = express();
 
 
@@ -24,6 +21,15 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+// ----------Add cookies and session-- 
+app.use(session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 // --- setting up the MongoDB --
@@ -33,6 +39,7 @@ mongoose.connect("mongodb://localhost:27017/userDB", () => {
     console.log("Connected to MongoDB");
 });
 
+
 // user schema AND creating new user (model)
 const Schema = mongoose.Schema;
 
@@ -40,80 +47,65 @@ const userSchema = new Schema({
     email: String,
     password: String
 });
+// set passportLocalMoogoes 
+userSchema.plugin(passportLocalMongoose);
 
-// Encription
-// convient method
-const secretENV = process.env.SECRET;
-
-userSchema.plugin(encrypt,
-
-    {
-
-        secret: secretENV,
-        encryptedFields: ["password"]
-    }
-);
 
 const User = new mongoose.model("User", userSchema); // after encription
+
+// initialize passport
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 // --- Routing to pages ---- 
 
 app.get("/", (req, res) => {
     res.render("home")
 });
 
-// login page
+// routing to login page
 app.route("/login")
     .get((req, res) => {
         res.render("login")
     })
     .post((req, res) => {
 
-        const username = req.body.username;
-        const password = req.body.password;
 
-        User.findOne(
-            { email: username }, (err, foundUser) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    if (foundUser) {
-                        bcrypt.compare(password, foundUser.password, (err, result) => {
-                            if (result === true) {
-                                res.render("secrets")
-                            }
-                        });
-                    }
-                }
-            });
     });
-// register 
+// routing to secrets page 
+app.route("/secrets")
+    .get((req, res) => {
+        if (req.isAuthenticated()){
+            res.render("secrets");
+        }else{
+            res.redirect("/login")
+        }
+    });
+
+// routing to register  page 
 app.route("/register")
     .get((req, res) => {
         res.render("register")
     })
     .post((req, res) => {
 
-        bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-            const newUser = new User(
-                {
-                    email: req.body.username,
-                    password: hash
-                });
-            newUser.save(
-                (err) => {
-                    if (!err) {
-                        res.render("secrets")
-                    } else {
-                        console.log(err)
-                    }
-                });
-
-        });
-
+        User.register(
+            {
+                username: req.body.username
+            },
+            req.body.password,
+            (err, user) => {
+                if (err) {
+                    console.log(err);
+                    res.redirect("/register")
+                } else {
+                    passport.authenticate("local")(req, res, () => {
+                        res.redirect("/secrets")
+                    })
+                }
+            });
     });
-
-
-
 
 // ------------------------HOST--------------------------------------
 // Host port 
