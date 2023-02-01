cc31f784ec0f4819cf6f7032f518a7e1e0fb3ffb
Ochwada <nakhulolinda@yahoo.com>
2023-02-01 16:45:57 +0100

    Google authentication

--- Authentication/app.js
+++ Authentication/app.js
@@ -61,6 +61,23 @@ passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// ---- google Authentication -----
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 // --- Routing to pages ---- 
 
 app.get("/", (req, res) => {
